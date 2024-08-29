b88cf65cf3c61a75b31bd9e8319ddf0d3cb89865
Santu506 <140848902+Santu506@users.noreply.github.com>
2024-08-29 22:58:16 +0530

    Update LatestCollections.jsx

--- src/components/LatestCollections.jsx
+++ src/components/LatestCollections.jsx
@@ -15,7 +15,7 @@ function LatestCollections() {
     <div className='my-10'>
         <div className='text-center py-8 text-3xl'>
             <Title text1={'Latest'} text2={'Collections'}/>
-            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base test-gray-600'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base test-gray-600'>Discover our newest fashion arrivals, where style meets sophistication. Our latest collections showcase the season's most coveted trends and timeless classics, designed to elevate your wardrobe with fresh, innovative looks. From chic everyday essentials to statement pieces perfect for special occasions, each garment is crafted with quality and attention to detail. Explore now to find the perfect outfit that expresses your unique style and keeps you at the forefront of fashion. </p>
         </div>
 
         {/* rendering product */}
